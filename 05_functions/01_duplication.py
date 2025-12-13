b2361d8a3a9f3cc8073e111e564bd6d8bad8a67a
SAI KIRAN <saikiran08643@gmail.com>
2025-12-13 14:46:25 +0530

    few files are modified and added in the repos for functions

--- 05_functions/01_duplication.py
+++ 05_functions/01_duplication.py
@@ -1,4 +1,5 @@
-
+# with the help of the function print_order, we can print the order details for different customers
+# with out writing same print statement multiple times
 
 def print_order(name ,order):# paramterized function to print order details
     print(f"The customer {name} has ordered {order}")
@@ -6,4 +7,4 @@ def print_order(name ,order):# paramterized function to print order details
 print_order("sai","chicken") # calling function with arguments
 print_order("kiran","panner") # calling function with arguments
 print_order("chebrolu","KFC") # calling function with arguments
-print_order("venkata","MCD") # calling function with arguments
+print_order("venkata","MCD") # calling function with argumentsprint_order("sai","chicken") # calling function with arguments
