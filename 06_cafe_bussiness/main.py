af4b7045e6d28fa36bb3389e181dd64c21fda854
SAI KIRAN <saikiran08643@gmail.com>
2025-12-15 08:59:52 +0530

    created a sample folder for learning importing functions and various ways of doing it

---
+++ 06_cafe_bussiness/main.py
@@ -0,0 +1,28 @@
+# we have multiple types of the importing methods here we are trying
+
+# import recipes.flavours
+
+# # print(recipes.flavours.cold_coffee())
+
+# print(dir(recipes.flavours))
+
+
+
+# from recipes.flavours import cold_coffee  # Only if function exists
+# print(cold_coffee())
+
+# from recipes.flavours import cold_coffee,matcha,espresso
+
+# print(cold_coffee())
+
+# print(matcha())
+
+# print(espresso())
+
+# import recipes.flavours
+
+# print(recipes.flavours.cold_coffee())
+
+# from recipes.flavours import cold_coffee as cf
+
+# print(cf())
---
+++ 06_cafe_bussiness/recipes/flavours.py
@@ -0,0 +1,8 @@
+def cold_coffee():
+    return "cold coffee is ready! enjoy your drink"
+
+def matcha():
+    return "matcha is ready! enjoy your drink"
+
+def espresso():
+    return "espresso is ready! enjoy your drink"
