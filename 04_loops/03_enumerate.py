24db71001bb411455d119638cf581324d41d51ce
SAI KIRAN <saikiran08643@gmail.com>
2025-12-15 12:45:20 +0530

    added text to enumerate file

--- 04_loops/03_enumerate.py
+++ 04_loops/03_enumerate.py
@@ -1,4 +1,7 @@
+# enumerate basically return the index and in a form of tuple
 items = ["snacks","drinks","food","tea","deserts"]
 
+
+# using for loop to iterate over the items
 for idx,snacks in enumerate(items,start=1):
     print(f"{idx} : {snacks}")
